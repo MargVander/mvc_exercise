c808fd16948f8f135a480fb04f0dc667c8f27292
MargVander <margauxvanderbeken@gmail.com>
2019-04-08 12:10:15 +0200

    Modification of the update method for item

--- app/controllers/administration/items_controller.rb
+++ app/controllers/administration/items_controller.rb
@@ -8,6 +8,13 @@ module Administration
     end
 
     def update
+      puts "%%%%%%%%%%%%%%%%%%%%%%%%%"
+      puts params
+      puts "%%%%%%%%%%%%%%%%%%%%%%%%%"
+      @item = Item.find(params["id"])
+      @item.update(discount_percentage: params["item"]["discount_percentage"], has_discount: true)
+      puts @item.original_price
+
       redirect_to administration_items_path
     end
   end
