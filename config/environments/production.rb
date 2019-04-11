af7f123b31df455c00f768b32c6646a13b340070
MargVander <margauxvanderbeken@gmail.com>
2019-04-11 16:06:53 +0200

    changed link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "mvc_exercise_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -95,12 +95,12 @@ Rails.application.configure do
   # MAILER
   config.action_mailer.default_url_options = { :host => 'price-exercise.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
-    :user_name => ENV["SENDGRID_USERNAME"],
-    :password => ENV["SENDGRID_PASSWORD"],
-    :domain => ENV["HOST"],
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    user_name: ENV['SENDGRID_LOGIN'],
+    password: ENV['SENDGRID_PWD'],
+    domain: 'price-exercise.herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 end
