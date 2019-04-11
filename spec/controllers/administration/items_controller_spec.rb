f0c5025c3226ac247b7588484cf992f1989b9b9b
MargVander <margauxvanderbeken@gmail.com>
2019-04-11 22:25:57 +0200

    spec modification

--- spec/controllers/administration/items_controller_spec.rb
+++ spec/controllers/administration/items_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Administration::ItemsController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:found)
     end
   end
 
