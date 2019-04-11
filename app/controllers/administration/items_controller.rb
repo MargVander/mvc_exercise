d419b193b856473e2ed524987c216b56d79c798b
MargVander <margauxvanderbeken@gmail.com>
2019-04-11 22:09:24 +0200

    modification of user info in item view

--- app/controllers/administration/items_controller.rb
+++ app/controllers/administration/items_controller.rb
@@ -3,6 +3,7 @@
 module Administration
   class ItemsController < AdministrationController
     def index
+      @users = User.all
       @emails = User.emails_of_all_users
       @items = Item.all
     end
