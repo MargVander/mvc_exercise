bdfa055ac51a6b14e27480ac198183760b293a82
MargVander <margauxvanderbeken@gmail.com>
2019-04-12 14:19:56 +0200

    updated seed to add category

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
+1.upto(3) do |i|
+  Category.create!(
+    name: Faker::Food.dish,
+    description: Faker::Food.description
+  )
+  p "CATEGORY #{i} : créé"
+end
+
 1.upto(10) do |i|
   Item.create!(
-    original_price: Faker::Number.decimal(2)
+    original_price: Faker::Number.decimal(2),
+    name: Faker::Food.fruits,
+    category: Category.all.sample
   )
   p "ITEM #{i} : créé"
 end
