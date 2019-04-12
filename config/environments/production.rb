caa3b9d38d1e06992b2f8e3b38e8dbf7448090d9
MargVander <margauxvanderbeken@gmail.com>
2019-04-12 15:27:47 +0200

    changed host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # MAILER
-  config.action_mailer.default_url_options = { :host => 'price-exercise.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => ENV["HOST"] }
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_LOGIN'],
     password: ENV['SENDGRID_PWD'],
